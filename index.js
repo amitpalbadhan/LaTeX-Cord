89ee19675ff53420da9261c1053e093e6bc2544c
Amitpal Badhan <amitpalsbadhan@gmail.com>
2021-02-26 17:46:22 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class LaTeXcord extends Plugin {
+  startPlugin () {
+    const domain = "http://chart.apis.google.com/chart?cht=tx&chl=";
+    powercord.api.commands.registerCommand({
+      command: 'latex',
+      description: 'Quickly compiled LaTeX in current channel.',
+      usage: '..',
+      executor: (args) => ({
+        send: true,
+        result: domain + encodeURIComponent(args.join(''))
+      })
+    });
+  }
+
+  pluginWillUnload () {
+    powercord.api.commands.unregisterCommand('latex');
+  }
+};
